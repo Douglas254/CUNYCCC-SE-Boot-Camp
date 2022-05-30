a62a34102ef8dde72a299cda23e90bb2a75afc6d
Douglas Obara <douglasobara97@gmail.com>
2022-05-30 21:55:07 +0300

    Script insert data in table

---
+++ Classwork_Practice/Insert data scripts/INSERT+INTO+movie_revenues+script.sql
@@ -0,0 +1,121 @@
+{\rtf1\ansi\deff3\adeflang1025
+{\fonttbl{\f0\froman\fprq2\fcharset0 Times New Roman;}{\f1\froman\fprq2\fcharset2 Symbol;}{\f2\fswiss\fprq2\fcharset0 Arial;}{\f3\froman\fprq2\fcharset0 Liberation Serif{\*\falt Times New Roman};}{\f4\fswiss\fprq2\fcharset0 Liberation Sans{\*\falt Arial};}{\f5\froman\fprq2\fcharset0 Helvetica{\*\falt Arial};}{\f6\fnil\fprq2\fcharset0 Noto Sans CJK SC;}{\f7\fnil\fprq2\fcharset0 Lohit Devanagari;}{\f8\fnil\fprq0\fcharset128 Lohit Devanagari;}}
+{\colortbl;\red0\green0\blue0;\red0\green0\blue255;\red0\green255\blue255;\red0\green255\blue0;\red255\green0\blue255;\red255\green0\blue0;\red255\green255\blue0;\red255\green255\blue255;\red0\green0\blue128;\red0\green128\blue128;\red0\green128\blue0;\red128\green0\blue128;\red128\green0\blue0;\red128\green128\blue0;\red128\green128\blue128;\red192\green192\blue192;}
+{\stylesheet{\s0\snext0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1 Normal;}
+{\s15\sbasedon0\snext16\dbch\af6\dbch\af7\afs28\sb240\sa120\keepn\loch\f4\fs28 Heading;}
+{\s16\sbasedon0\snext16\sl276\slmult1\sb0\sa140 Text Body;}
+{\s17\sbasedon16\snext17\dbch\af8\sl276\slmult1\sb0\sa140 List;}
+{\s18\sbasedon0\snext18\dbch\af8\afs24\ai\sb120\sa120\noline\fs24\i Caption;}
+{\s19\sbasedon0\snext19\dbch\af8\noline Index;}
+}{\*\generator LibreOffice/6.4.7.2$Linux_X86_64 LibreOffice_project/40$Build-2}{\info{\creatim\yr0\mo0\dy0\hr0\min0}{\revtim\yr0\mo0\dy0\hr0\min0}{\printim\yr0\mo0\dy0\hr0\min0}}{\*\userprops}\deftab720
+\hyphauto1\viewscale100
+{\*\pgdsctbl
+{\pgdsc0\pgdscuse451\pgwsxn11906\pghsxn16838\marglsxn1440\margrsxn1440\margtsxn1440\margbsxn1440\pgdscnxt0 Default Style;}}
+\formshade{\*\pgdscno0}\paperh16838\paperw11906\margl1440\margr1440\margt1440\margb1440\sectd\sbknone\sectunlocked1\pgndec\pgwsxn11906\pghsxn16838\marglsxn1440\margrsxn1440\margtsxn1440\margbsxn1440\ftnbj\ftnstart1\ftnrstcont\ftnnar\aenddoc\aftnrstcont\aftnstart1\aftnnrlc
+{\*\ftnsep\chftnsep}\pgndec\pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+INSERT INTO movie_revenues (revenue_id, movie_id, domestic_takings, international_takings) VALUES ('1','45','22.2','1.3'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('2','13','199.4','201.2'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('3','23','102.1',null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('4','44','158.7',null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('6','1','27.1',null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('7','53',null,null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('17','18','260.3','210.9'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('9','39','28.1',null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('5','35','461.2','314.2'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('13','2','83.4',null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('15','21','19.6',null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('8','36','290.3','247.8'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('11','43','44.1',null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('12','29','23.1',null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('14','4','33.3',null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('10','37','309.1','166.2'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('16','49','180.1','177.3'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('18','14','46.6',null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('21','11','330.3','348.1'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('20','28','107.9','106.2'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('19','19',null,null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('23','50','192.1','182.4'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('22','5',null,null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('27','41','64.1','200.3'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('24','48','659.2','1528.1'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('25','30','16.9',null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('26','10','55.7','106.3'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('30','12','188.2','273.4'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('28','9','128.1','85.1'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('29','3',null,null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('32','17','2.9','10.2'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('31','34','11.1','265.4'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('33','31','404.1','418.1'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('37','6','8.2','23.5'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('35','16','11.1','82.5'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('36','32','374.1','410.4'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('34','25','1.1','13.8'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('38','51','71.2','62.5'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('40','26','37.8','46.4'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('48','42','11.3','66.1'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('39','33','337','554'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('51','40','11.9','23.2'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('41','46','39.9','35.8'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('42','7','0.3','2.2'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('49','20','2.1','9.1'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('45','52','107.5','77.5'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('44','27','15.1','186.7'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('47','8',null,null),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('46','24','2.4','7.1'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('43','38','0.5','0.4'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('50','22','124.9','484.1'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('52','15','59.3','115.5'),}
+\par \pard\plain \s0\hich\af3\dbch\af9\langfe2052\dbch\af7\afs24\alang1081\nowidctlpar\hyphpar0\ltrpar\cf0\loch\f3\fs24\lang1033\kerning1\ql\ltrpar{\rtlch\hich\af5 \ltrch\cf0\fs24\loch\f5\loch
+('53','47','54.5','104.7');}
+\par }
